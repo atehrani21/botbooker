7617a2fe590599a93d83aae117f423c09e2d0221
Joseph Schwarz <josephschwarz@Josephs-MacBook-Pro-3.local>
2016-09-25 12:06:17 -0400

    fix

--- server/bot/bot.js
+++ server/bot/bot.js
@@ -79,8 +79,8 @@ module.exports = {
                 "payload": "Payload for first element in a generic bubble",
               }]
           });
-        })
-      }
+        });
+
       request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {
