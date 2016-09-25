86d64235b0fbd0ff5ae8c0f0a3afbe4c6358e731
Joseph Schwarz <josephschwarz@Josephs-MacBook-Pro-3.local>
2016-09-25 08:43:48 -0400

    fix seed bug

--- server/bot/bot.js
+++ server/bot/bot.js
@@ -115,7 +115,6 @@ module.exports = {
         messageData.quick_replies.push({
           "content_type": "text",
           "title": `${user["firstname"]} ${user["lastname"]}`,
-          "user_id": user["_id"],
           "payload": `DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_${user["firstname"].toUpperCase()}`
         })
       });
